a27c21a0b055290be96fe27e368e79a2556446e8
NayanVR <nvradadiya281@gmail.com>
2022-06-30 08:38:10 +0530

    Project is moved from next to react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,26 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './styles/App.css'
+import './styles/welcome.css'
+import Welcome from './layouts/welcome.jsx'
+import Credentials from './layouts/credentials.jsx'
+import Game from './layouts/game.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const components = {
+    welcome : <Welcome clickToContinue={clickToContinue}/>,
+    credentials: <Credentials />,
+    game: <Game />
+  }
+  
+  const [renderComp, setRenderComp] = useState(components.welcome)
+  
+  function clickToContinue() { setRenderComp(components.credentials) }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      {renderComp}
+    </>
   )
 }
 
---
+++ client/src/layouts/welcome.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/welcome.css'
+import '../styles/typography.css'
+import '../styles/buttons.css'
+
+export default function Welcome( {clickToContinue} ) {
+    return (
+        <main className="main-container">
+            <div className="welcome-title">
+                Welcome to    
+            </div>
+            <div className="playful-heading pong-title">
+                PONG!
+            </div>
+            <button onClick={() => {clickToContinue()}} className="btn-primary cta-btn">Click to continue</button>
+        </main>
+    )
+}
